2f1aec90ec51bf25794ec1d808bbcbbf78d8186d
candicechretien <candice5172@yahoo.fr>
2018-11-27 10:14:18 +0100

    exo lesson 1 et 2

---
+++ javascript/lesson2/exercise1/exercise/main.js
@@ -0,0 +1,11 @@
+var heure = 8;
+
+if (heure<10) {
+    console.log("le matin");
+}
+else if (heure<20) {
+    console.log("l'après midi");
+}
+else {
+    console.log("le soir");
+}
---
+++ javascript/lesson2/exercise2/exercise/main.js
@@ -0,0 +1,11 @@
+for ( var i = 0 ; i < 100 ; i++ ) {
+    console.log("Je compte jusqu'à " + i);
+}
+
+for ( var i = 0 ; i < 100 ; i = i + 2 ) {
+    console.log("Je compte jusqu'à " + i);
+}
+
+for ( var i = 100 ; i > 0 ; i = i - 2 ) {
+    console.log("Je compte jusqu'à " + i);
+}
