53c946699ac080b54a4729445023f8f74ef88690
candicechretien <candice5172@yahoo.fr>
2018-12-07 15:42:32 +0100

    exercices non corrigés

---
+++ javascript/lesson5/exercise1/main.js
@@ -0,0 +1,15 @@
+var texte = document.getElementById("texte");
+texte.innerHTML ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+
+function afficher() {
+
+texte.style.display = "block";
+
+}
+function masquer() {
+    texte.style.display = "none";
+
+function couleur(){
+    var tab = ["blue";"red";"green"]
+    
+}
---
+++ javascript/lesson5/exercise2/exercise/main.js
@@ -0,0 +1,11 @@
+function exercise2() {
+    var div1 = document.createElement("div");
+    document.body.appendChild(div1);
+    div1.style.backgroundcolor = "red";
+    div1.style.height = "350px";
+    div1.style.width = "200px";
+
+    crt.mouse
+}
+
+exercise2();
