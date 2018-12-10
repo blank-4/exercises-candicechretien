a1c3f70a1bddb243283fcc9f7b5e30c784d2516d
candicechretien <44778668+candicechretien@users.noreply.github.com>
2018-12-10 10:55:28 +0100

    Lesson4 javascript (#9)
    * exercise lesson 3
    
    * exercise 1 lesson 4

---
+++ javascript/lesson4/exercise1/main.js
@@ -0,0 +1,17 @@
+function exercise1(){
+    var inventaire=["salade", "tomate", "oignons"];
+    console.log( inventaire.length);
+    console.log(inventaire[0]);
+    inventaire.push("sauce blanche");
+   console.log( inventaire.length);
+    inventaire.pop("sauce blanche");
+    afficher(inventaire);
+
+}
+
+exercise1();
+
+    function afficher(tab) {
+        for var i=0;i<inventaire.lenght; i++) 
+
+    }
