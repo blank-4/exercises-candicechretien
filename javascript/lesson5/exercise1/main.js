2e1a07ce0f5ebc8938e32152bc90e1fe5105ba17
Marin Procureur <marin.procureur@gmail.com>
2018-12-10 10:55:39 +0100

    exercices non corrigés (#10)
    * exercices non corrigés
    
    * exrecise 1 lesson5
    
    * exercise 2
    
    * exercise

---
+++ javascript/lesson5/exercise1/main.js
@@ -0,0 +1,18 @@
+var texte = document.getElementById("texte");
+texte.innerHTML ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+
+function afficher() {
+
+texte.style.display = "block";
+
+}
+function masquer() {
+    texte.style.display = "none";
+
+function generateColor() {
+    var r = Math.floor(Math.random( ) *256);
+    var g= Math.floor(Math.random( ) *256);
+    var b = Math.floor(Math.random( ) *256);
+    
+    
+}
---
+++ javascript/lesson5/exercise2/exercise/main.js
@@ -0,0 +1,12 @@
+function exercise2() {
+    var div1 = document.createElement("div");
+    document.body.appendChild(div1);
+    div1.style.backgroundcolor = "red";
+    div1.style.height = "350px";
+    div1.style.width = "202px";
+
+    div.onmouseout = function() {}
+    div.style.backgrounColor = "red";
+}
+
+exercise2();
